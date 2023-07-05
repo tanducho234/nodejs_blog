fe7ad87d172175f57b2f186a25451a1ee0083a12
Tangit <tanhdgcd201919@fpt.edu.vn>
2023-07-05 15:20:32 +0700

    MCV complete

--- src/index.js
+++ src/index.js
@@ -6,26 +6,29 @@ const exphbs  = require('express-handlebars');
 const app = express()
 const port = 3000
 
+const route = require('./routes') //./routes/index.js
+
 app.use(express.static(path.join(__dirname,'public')))
 
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(express.json())
+
 //HTTP logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 //Template engine
 app.engine('hbs', exphbs({
   extname: '.hbs'
 }));
+
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname,'resources/views'))
 
+//Route init
+route(app)
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
-
-app.get('/news', (req, res) => {
-  res.render('news')
-})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 }) 
---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+const newsRouter = require('./news') // ./news.js
+const siteRouter = require('./site')
+
+function route(app) {
+    
+    app.use('/news',newsRouter)
+
+    app.use('/',siteRouter)
+
+   
+}
+module.exports = route
